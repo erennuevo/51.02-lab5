b81c22c3be331c52cf5a7b7f6191f1aae38ea6e1
erennuevo <lib.nuevo@student.ateneo.edu>
2026-02-25 22:20:39 +0800

    Update multiplyByX.cpp

--- multiplyByX.cpp
+++ multiplyByX.cpp
@@ -1,15 +1,17 @@
 #include "IntArray.h"
 
-// void multiplyByX(IntArray *p, int x)
-// {
-//     for (int i = 0; i < p->size; i++) {
-//         p->elements[i] *= x;
-//     }
-// }
-
-void multiplyByX(IntArray *p)
+void multiplyByX(IntArray *p, int x)
 {
     for (int i = 0; i < p->size; i++) {
-        p->elements[i] *= 61;
+        p->elements[i] *= x;
     }
 }
+
+// multiplyBy61 to generate assembly template
+
+// void multiplyByX(IntArray *p)
+// {
+//     for (int i = 0; i < p->size; i++) {
+//         p->elements[i] *= 61;
+//     }
+// }
